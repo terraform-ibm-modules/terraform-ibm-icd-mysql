1f694c12a0342d230407f4329348991e0856db4d
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2024-06-01 05:54:39 +0000

    fix(deps): update terraform-module

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -16,7 +16,7 @@ module "resource_group" {
 
 module "key_protect_all_inclusive" {
   source            = "terraform-ibm-modules/key-protect-all-inclusive/ibm"
-  version           = "4.11.8"
+  version           = "4.13.1"
   resource_group_id = module.resource_group.resource_group_id
   # Note: Database instance and Key Protect must be created in the same region when using BYOK
   # See https://cloud.ibm.com/docs/cloud-databases?topic=cloud-databases-key-protect&interface=ui#key-byok
@@ -74,7 +74,7 @@ resource "time_sleep" "wait_30_seconds" {
 
 module "cbr_zone" {
   source           = "terraform-ibm-modules/cbr/ibm//modules/cbr-zone-module"
-  version          = "1.22.0"
+  version          = "1.22.1"
   name             = "${var.prefix}-VPC-network-zone"
   zone_description = "CBR Network zone representing VPC"
   account_id       = data.ibm_iam_account_settings.iam_account_settings.account_id
@@ -137,7 +137,7 @@ resource "time_sleep" "wait_120_seconds" {
 
 module "vpe" {
   source  = "terraform-ibm-modules/vpe-gateway/ibm"
-  version = "4.1.1"
+  version = "4.1.3"
   prefix  = "vpe-to-my"
   cloud_service_by_crn = [
     {
