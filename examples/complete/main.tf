9930f233ef1aff106af013a6e291086b7011a87f
Todd Giguere <todd.giguere@ibm.com>
2024-07-02 11:11:56 -0400

    test: removed configuration from complete example

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -107,9 +107,6 @@ module "mysql_db" {
   resource_tags              = var.resource_tags
   service_credential_names   = var.service_credential_names
   access_tags                = var.access_tags
-  configuration = {
-    max_connections = 250
-  }
   cbr_rules = [
     {
       description      = "${var.prefix}-mysql access only from vpc"
