ae632893609bfbc81cb55893c8dc243600bb49a5
shemau <shemau@gmail.com>
2024-07-11 17:32:12 +0100

    feat: support for new ICD hosting model (#92)

--- examples/fscloud/main.tf
+++ examples/fscloud/main.tf
@@ -65,6 +65,7 @@ module "mysql_db" {
   service_credential_names   = var.service_credential_names
   access_tags                = var.access_tags
   auto_scaling               = var.auto_scaling
+  member_host_flavor         = "b3c.4x16.encrypted"
   backup_encryption_key_crn  = var.backup_encryption_key_crn
   backup_crn                 = var.backup_crn
   cbr_rules = [
