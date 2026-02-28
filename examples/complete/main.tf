0a5610808191e9942f85e4a1a7446f2f78ca1d1d
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-02-28 04:21:25 +0000

    fix(deps): update dependencies (#429)

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -87,7 +87,7 @@ resource "time_sleep" "wait_30_seconds" {
 
 module "cbr_zone" {
   source           = "terraform-ibm-modules/cbr/ibm//modules/cbr-zone-module"
-  version          = "1.35.15"
+  version          = "1.35.16"
   name             = "${var.prefix}-VPC-network-zone"
   zone_description = "CBR Network zone representing VPC"
   account_id       = data.ibm_iam_account_settings.iam_account_settings.account_id
@@ -160,7 +160,7 @@ resource "time_sleep" "wait_120_seconds" {
 
 module "vpe" {
   source  = "terraform-ibm-modules/vpe-gateway/ibm"
-  version = "5.0.4"
+  version = "5.0.5"
   prefix  = "vpe-to-my"
   cloud_service_by_crn = [
     {
