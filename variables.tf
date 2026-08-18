67878e1bd397e26b3587294b495cd29ee2bbf789
zikra-iqbal <zikra.iqbalmnnit@gmail.com>
2026-08-18 18:01:50 +0530

    chore: temporary changes for testing da

--- variables.tf
+++ variables.tf
@@ -435,6 +435,7 @@ variable "kms_key_crn" {
     condition = anytrue([
       var.kms_key_crn == null,
       can(regex(".*kms.*", var.kms_key_crn)),
+      can(regex(".*hs-crypto.*", var.kms_key_crn)),
     ])
     error_message = "Value must be the KMS key CRN from a Key Protect instance."
   }
@@ -460,6 +461,7 @@ variable "backup_encryption_key_crn" {
     condition = anytrue([
       var.backup_encryption_key_crn == null,
       can(regex(".*kms.*", var.backup_encryption_key_crn)),
+      can(regex(".*hs-crypto.*", var.backup_encryption_key_crn)),
     ])
     error_message = "Value must be the KMS key CRN from a Key Protect instance in one of the supported backup regions."
   }
